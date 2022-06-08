e4c22cf7d6d72a0e338be4fad88d2cd91e09e5f5
sxcar98 <sxcar98@gmail.com>
2022-06-09 07:58:01 +0800

    macOS configs

---
+++ chadrc.lua
@@ -0,0 +1,51 @@
+local opt = vim.opt
+local override = require "custom.override"
+
+local M = {}
+
+M.options = {
+
+   user = function()
+      opt.tabstop = 4
+      opt.shiftwidth = 4
+      opt.mouse = "c"
+      opt.expandtab = false
+   end,
+}
+
+M.plugins = {
+
+   options = {
+      lspconfig = {
+         setup_lspconf = "custom.plugins.lspconfig",
+      },
+
+      statusline = {
+         separator_style = "default",
+      },
+   },
+
+   override = {
+      ["kyazdani42/nvim-tree.lua"] = override.nvimtree,
+      ["nvim-treesitter/nvim-treesitter"] = override.treesitter,
+      ["lukas-reineke/indent-blankline.nvim"] = override.blankline,
+      ["hrsh7th/nvim-cmp"] = override.cmp,
+      ["williamboman/nvim-lsp-installer"] = override.lsp_installer,
+   },
+
+   user = require "custom.plugins",
+
+   remove = {},
+}
+
+M.ui = {
+   hl_override = require "custom.highlights",
+   changed_themes = {},
+   theme_toggle = { "onedark", "one_light" },
+   theme = "gruvchad",
+   transparency = true,
+}
+
+M.mappings = require "custom.mappings"
+
+return M
